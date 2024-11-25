5eb66f78d033d007e08cf5435c8776e9ff1be1bb
Nuno Campos <nuno@langchain.dev>
2024-11-25 14:21:44 -0800

    Add graph def

--- ch10/py/graph.py
+++ ch10/py/graph.py
@@ -2,6 +2,7 @@ from typing import List, TypedDict
 
 from langchain_core.schema import Document
 from langchain_community.tools.tavily_search import TavilySearchResults
+from langgraph.graph import END, StateGraph, START
 
 from .grader import retrieval_grader
 from .retriever import retriever
@@ -165,3 +166,33 @@ def decide_to_generate(state):
     else:
         # We have relevant documents, so generate answer
         return "generate"
+
+
+### Graph
+
+workflow = StateGraph(GraphState)
+
+# Define the nodes
+workflow.add_node("retrieve", retrieve)  # retrieve
+workflow.add_node("grade_documents", grade_documents)  # grade documents
+workflow.add_node("generate", generate)  # generatae
+workflow.add_node("transform_query", transform_query)  # transform_query
+workflow.add_node("web_search_node", web_search)  # web search
+
+# Build graph
+workflow.add_edge(START, "retrieve")
+workflow.add_edge("retrieve", "grade_documents")
+workflow.add_conditional_edges(
+    "grade_documents",
+    decide_to_generate,
+    {
+        "transform_query": "transform_query",
+        "generate": "generate",
+    },
+)
+workflow.add_edge("transform_query", "web_search_node")
+workflow.add_edge("web_search_node", "generate")
+workflow.add_edge("generate", END)
+
+# Compile
+app = workflow.compile()
