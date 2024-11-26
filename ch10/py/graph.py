9a4cc39b05cd93a3c89da799a72c07be1e8f6947
Mayo Oshin <107035552+mayooear@users.noreply.github.com>
2024-11-26 16:55:52 +0000

    Fix: Add the question re-writer in graph.py

--- ch10/py/graph.py
+++ ch10/py/graph.py
@@ -8,6 +8,31 @@ from .grader import retrieval_grader
 from .retriever import retriever
 from .rag import rag_chain
 
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+
+
+### Question Re-writer
+
+# LLM
+llm = ChatOpenAI(model="gpt-4", temperature=0)
+
+# Prompt
+system = """You a question re-writer that converts an input question to a better version that is optimized \n 
+     for web search. Look at the input and try to reason about the underlying semantic intent / meaning."""
+re_write_prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", system),
+        (
+            "human",
+            "Here is the initial question: \n\n {question} \n Formulate an improved question.",
+        ),
+    ]
+)
+
+question_rewriter = re_write_prompt | llm | StrOutputParser()
 
 # --- Create the graph ---
 
