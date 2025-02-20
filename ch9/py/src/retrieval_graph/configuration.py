8cdd17a165c7d91d55d3788ca1e7bad899f3a52d
mayo <mayo@siennaianalytics.com>
2025-02-20 21:46:47 +0000

    fix: update root README.md

---
+++ ch9/py/src/retrieval_graph/configuration.py
@@ -0,0 +1,21 @@
+"""Define the configurable parameters for the agent."""
+
+from __future__ import annotations
+
+from dataclasses import dataclass, field, fields
+from typing import Annotated, Any, Literal, Optional, Type, TypeVar
+
+from langchain_core.runnables import RunnableConfig, ensure_config
+from shared.configuration import BaseConfiguration
+
+
+@dataclass(kw_only=True)
+class Configuration(BaseConfiguration):
+    """The configuration for the agent."""
+
+    query_model: Annotated[str, {"__template_metadata__": {"kind": "llm"}}] = field(
+        default="openai/gpt-4o",
+        metadata={
+            "description": "The language model used for processing and refining queries. Should be in the form: provider/model-name."
+        },
+    )
